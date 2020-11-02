2a2fc39e95011a8c8413e521adac62bd10919cce
alexite03 <snipescope360@gmail.com>
2020-11-02 15:32:02 +0100

    final iteration

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Read instructions. And remove this line.
+const randomValue = Math.floor(Math.random() * 100)
+console.log(randomValue)
+
+function getUserGuess() {
+    const stringValue = document.getElementById('user-input').value
+    return parseInt(stringValue)
+}
+
+function setMessage(msg){
+    document.getElementById('message').innerText = msg
+}
+
+addEventListener("keyup", 
+function(event){
+if(event.key === "Enter"){
+const guess = getUserGuess()
+
+    if(randomValue === guess){
+    setMessage("Correct!")
+    }
+    else if(randomValue < guess){
+    setMessage("Too High!")
+    document.getElementById('user-input').value = ''
+    }
+    else if(randomValue > guess){
+    setMessage("Too Low!")
+    document.getElementById('user-input').value = ''
+    }
+
+}})
